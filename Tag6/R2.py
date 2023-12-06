8198c55ff66f2288585e1ab5468086cf1c561a9c
coderJakub <kliemann.jakub@gmail.com>
2023-12-06 13:39:37 +0100

    Tag6 done

--- Tag6/R2.py
+++ Tag6/R2.py
@@ -16,17 +16,23 @@ times[-1]=times[-1].split("\n")[0]
 distance[-1]=distance[-1].split("\n")[0]
 
 res=1
-
+timeS=""
+distanceS=""
 for i,time in enumerate(times):
-    startB=0
-    endB=0
-    for j in range(int(time)):
-        if func(j,int(time))>int(distance[i]):
-            startB=j
-            break
-    for j in range(int(time),startB,-1):
-        if func(j,int(time))>int(distance[i]):
-            endB=j
-            break
-    res*=(endB-startB)+1
+    timeS+=time
+    distanceS+=distance[i]
+print(timeS, distanceS)
+startB=0
+endB=0
+time=int(timeS)
+distance=int(distanceS)
+for j in range(int(time)):
+    if func(j,int(time))>int(distance):
+        startB=j
+        break
+for j in range(int(time),startB,-1):
+    if func(j,int(time))>int(distance):
+        endB=j
+        break
+res*=(endB-startB)+1
 print(res)
