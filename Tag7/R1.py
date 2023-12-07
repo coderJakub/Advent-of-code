16a830d4038e74acdf9a8e58c7bc54e187bec233
coderJakub <kliemann.jakub@gmail.com>
2023-12-07 09:05:40 +0100

    getVal func for day7

--- Tag7/R1.py
+++ Tag7/R1.py
@@ -1,7 +1,37 @@
 
+def getValue(c):
+    twoPair1 ='.'
+    threePair = '.'
+    fourPair = '.'
+    twoPair2='.'
 
-def compare(c1,c2):
-    foundVal=False
-    for i in c1:
-        if i!=c1[1]:
-                    
+    for i in range(len(c)):
+        for j in range(i+1,len(c)):
+            if c[i]==c[j]:
+                if twoPair1==c[i]:
+                    if threePair==c[i]:
+                        if fourPair==c[i]:
+                            return 7
+                        else:
+                            fourPair=c[i]
+                    else:
+                        threePair=c[i]
+                elif twoPair2==c[i]:
+                    return 6
+                    twoPair=c[i]
+                elif twoPair1=='.':
+                    twoPair1=c[i]
+                else:
+                    twoPair2=c[i]
+    if twoPair1=='.':
+        return 1
+    if threePair=='.' and twoPair2=='.':
+        return 2
+    if threePair=='.' and twoPair2!='.':
+        return 3
+    if threePair!='.' and twoPair2=='.':
+        if fourPair=='.':
+            return 4
+        return 5
+    return 0
+
