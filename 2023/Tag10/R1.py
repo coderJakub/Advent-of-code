5feb2e429afb54cd87b62dbf5cad42bf007c5ff2
Jakub Kliemann <jakub.kliemann@stud.htw-dresden.de>
2023-12-13 16:57:42 +0100

    refined solutions

--- 2023/Tag10/R1.py
+++ 2023/Tag10/R1.py
@@ -1,77 +1,48 @@
 from sys import argv
 
 def getNextIdx(i,j):
-    if content[i][j]=='|':
-        return [[i+1,j],[i-1,j]]
-    elif content[i][j]=='-':
-        return [[i,j+1],[i,j-1]]
-    elif content[i][j]=='J':
-        return [[i-1,j], [i, j-1]]
-    elif content[i][j]=='L':
-        return [[i-1,j], [i, j+1]]
-    elif content[i][j]=='7':
-        return [[i+1,j], [i, j-1]]
-    elif content[i][j]=='F':
-        return [[i+1,j], [i, j+1]]
-    elif content[i][j]=='S' or content[i][j]=='.':
-        return None
+    match content[i][j]:
+        case '|':return [[i+1,j],[i-1,j]]
+        case '-':return [[i,j+1],[i,j-1]]
+        case 'J':return [[i-1,j],[i, j-1]]
+        case 'L':return [[i-1,j],[i, j+1]]
+        case '7':return [[i+1,j],[i, j-1]]
+        case 'F':return [[i+1,j],[i, j+1]]
+        case _:return None
 
 with open(argv[1]) as f:
     content = f.read().splitlines()
 
 resArr=[[-1 for j in range(len(content[i]))] for i in range(len(content))]
-lineIdx=0
-itemIdx=0
+startCoords=[0,0]
 for line, item in enumerate(content):
     if item.find('S')!=-1:
-        lineIdx=line
-        itemIdx=item.index('S')
+        startCoords=[line,item.index('S')]
         break
-resArr[lineIdx][itemIdx]=0
-start1=[-1,-1]
-start2=[-1,-1]
+resArr[startCoords[0]][startCoords[1]]=0
+currElement=[-1,-1]
 
-#get start1 and start2
-for i in range(lineIdx-1, lineIdx+2):
-    for j in range(itemIdx-1, itemIdx+2):
+#get currElement
+for i in range(startCoords[0]-1, startCoords[0]+2):
+    for j in range(startCoords[1]-1, startCoords[1]+2):
         if i>=0 and j>=0 and i<len(content) and j<len(content[0]):
-            x = getNextIdx(i,j)
-            if x!=None and [lineIdx, itemIdx] in x and start1==[-1,-1]:
-                start1=[i,j]
-            elif x!=None and [lineIdx, itemIdx] in x:
-                start2=[i,j]
+            if startCoords in getNextIdx(i,j):
+                currElement=[i,j]
                 break
-print("[",lineIdx, itemIdx,"]",start1, start2)
+
 i=1
-before = [lineIdx, itemIdx]
+before = startCoords
 #iterate from first way
-while start1!=start2:
-    resArr[start1[0]][start1[1]]=i
+while currElement!=startCoords:
+    resArr[currElement[0]][currElement[1]]=i
     i+=1
-    sol=getNextIdx(start1[0], start1[1])
+    sol=getNextIdx(currElement[0], currElement[1])
     if sol[0]==before:
-        before=start1
-        start1=sol[1]
+        before=currElement
+        currElement=sol[1]
     else:
-        before=start1
-        start1=sol[0]
-resArr[start1[0]][start1[1]]=i
-i=1
-before = [lineIdx, itemIdx]
-#iterate from second way
-print(resArr[start2[0]][start2[1]])
-while resArr[start2[0]][start2[1]]>i:
-    resArr[start2[0]][start2[1]]=i
-    i+=1
-    sol=getNextIdx(start2[0], start2[1])
-    if sol[0]==before:
-        before=start2
-        start2=sol[1]
-    else:
-        before=start2
-        start2=sol[0]
-x =-1
-for k in resArr:
-    if x<max(k):
-        x=max(k)
-print(x)
+        before=currElement
+        currElement=sol[0]
+resArr[currElement[0]][currElement[1]]=i
+
+print(resArr[startCoords[0]][startCoords[1]]//2)
