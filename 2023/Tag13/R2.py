3c3a3cb645f00f2082196c60194567fdbf207005
Jakub Kliemann <jakub.kliemann@stud.htw-dresden.de>
2023-12-13 15:59:48 +0100

    day 13 done

--- 2023/Tag13/R2.py
+++ 2023/Tag13/R2.py
@@ -10,29 +10,30 @@ res=0
 for content in data:
     for i in range(len(content)-1):
         j=0
-        err=False
+        err=0
         while True:
             if 0<=i-j<i+j+1<len(content):
-                if content[i-j] != content[i+j+1]:
-                    err=True
+                for k in range(len(content[0])):
+                    if content[i-j][k]!=content[i+j+1][k]:
+                        err+=1
                 j+=1
             else:
                 break
-        if not err:
+        if err==1:
             res+=(i+1)*100
 
     for i in range(len(content[0])-1):
         j=0
-        err=False
+        err=0
         while True:
             if 0<=i-j<i+j+1<len(content[0]):
                 for k in range(len(content)):
                     if content[k][i-j]!=content[k][i+j+1]:
-                        err=True
+                        err+=1
                 j+=1
             else: 
                 break
-        if not err:
+        if err==1:
             res+=i+1
             
 print(res)
