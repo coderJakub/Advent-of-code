f26e6e6625be2ccc6d5d53001518d429b1fb8b09
coderJakub <kliemann.jakub@gmail.com>
2024-12-04 21:09:17 +0100

    day14/2023 done

--- 2023/Tag14/R2.py
+++ 2023/Tag14/R2.py
@@ -1,44 +1,44 @@
 from sys import argv
-
+from copy import deepcopy
 with open(argv[1]) as f:
     content=f.read().splitlines()
 content = [list(i) for i in content]
-def cycle(l): 
-    for row,dataR in enumerate(content):
-        for col,dataC in enumerate(dataR):
-            if row==0:
-                break
-            if dataC=='O':
-                i=row-1
-                while i>=0:
-                    if content[i][col] in ['#', 'O']:
-                        break
-                    i-=1
-                i+=1
-                content[row][col] = '.'
-                content[i][col] = 'O'
-    for col in range(len(content[0])):
-        for row,dataR in enumerate(content):
-            if col==0:
-                break
-            if dataR[col]=='O':
-                i=col-1
-                while i>=0:
-                    if content[row][i] in ['#', 'O']:
-                        break
-                    i-=1
-                i+=1
-                content[row][col] = '.'
-                content[row][i] = 'O'
-    content.reverse()
-    for i in range(len(content)):
-        content[i].reverse()
-    if l==0:
-        cycle(1)
-        
-for i in range(3):
-    print(i)
-    cycle(0)
 
-for i in content:
-    print(i)
+R = len(content)
+C = len(content[0])
+
+def cycle(content): 
+    for _ in range(2):
+        for xx,yy in [(-1,0),(0,-1)]:
+            for row,dataR in enumerate(content):
+                for col,dataC in enumerate(dataR):
+                    if dataC=='O':
+                        i,j = row+xx,col+yy
+                        while i in range(R) and j in range(C):
+                            if content[i][j] in ['#', 'O']:
+                                break
+                            i,j = i+xx,j+yy
+                            
+                        i,j = i-xx,j-yy
+                        content[row][col] = '.'
+                        content[i][j] = 'O'
+        content = content[::-1]
+        for i in range(C):
+            content[i] = content[i][::-1]
+    return content
+
+cycles = [deepcopy(content)]
+cycleLen=0
+cycleStart=0
+for i in range(1,1000000001):
+    content = cycle(content)
+    if content in cycles:
+        cycleStart = cycles.index(content)
+        cycleLen = i-cycleStart
+        break
+    cycles.append(deepcopy(content))
+
+load = 0
+for row,dataR in enumerate(cycles[(1000000000-cycleStart)%cycleLen+cycleStart]):
+    load+=dataR.count("O")*(len(content)-row)
+print(load)
