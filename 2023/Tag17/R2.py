643dc3f525299db3ed06b2e1f787ae380708e025
coderJakub <kliemann.jakub@gmail.com>
2024-12-04 23:41:35 +0100

    d17/2023

--- 2023/Tag17/R2.py
+++ 2023/Tag17/R2.py
@@ -13,19 +13,18 @@ def bfs(grid, start):
     visited = {}
     while q:
         d,x,y,dir = q.pop(q.index(min(q, key=lambda x: x[0])))
-        if x == R-1 and y == C-1:
+        if x == R-1 and y == C-1 and dir[1]>=3:
             return d
         if (x,y,dir) in visited:
             continue
         visited[(x,y,dir)] = d
         for dr,dc in ((0,1),(1,0),(0,-1),(-1,0)):
-            if ((dr,dc) == dir[0] and dir[1] >= 2) or (dr,dc) == (-dir[0][0],-dir[0][1]):
+            if ((dr,dc) == dir[0] and dir[1] >= 9) or (dr,dc) == (-dir[0][0],-dir[0][1]) or ((dr,dc) != dir[0] and dir[0]!=(0,0) and dir[1]<3):
                 continue
             
             nr,nc = x+dr,y+dc
             ndir = ((dr,dc), 0 if (dr,dc) != dir[0] else dir[1]+1)
             if 0 <= nr < R and 0 <= nc < C and (nr,nc,ndir) not in visited:
                 q.append((d+grid[nr][nc],nr,nc,ndir))
-    return visited
         
 print(bfs(grid, (0,0)))
