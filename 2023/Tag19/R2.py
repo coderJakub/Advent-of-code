0e2955bb7a4e1c91c37130437d0864d9792977fd
Jakub Kliemann <114301287+coderJakub@users.noreply.github.com>
2024-12-07 18:52:45 +0000

    day19/2023 p2

--- 2023/Tag19/R2.py
+++ 2023/Tag19/R2.py
@@ -1,4 +1,5 @@
 from sys import argv
+from copy import deepcopy
 
 with open(argv[1]) as f:
     content = f.read()
@@ -6,75 +7,46 @@ with open(argv[1]) as f:
 fp = content.split('\n\n')[0]
 
 workflows = {line.split('{')[0]:line[:-1].split('{')[1] for line in fp.splitlines()}
+idx = {'x':0, 'm':1, 'a':2, 's':3}
 
-def interval_intersection(a, b):
-    result = []
-    i, j = 0, 0
+queue = [['in', [[1,4000],[1,4000],[1,4000],[1,4000]]]]
 
-    while i < len(a) and j < len(b):
-        start = max(a[i][0], b[j][0])
-        end = min(a[i][1], b[j][1])
+accepeted = []
 
-        if start <= end:
-            result.append([start, end])
-
-        if a[i][1] < b[j][1]:
-            i += 1
-        else:
-            j += 1
-
-    return result
-def interval_union(a, b):
-    intervals = sorted(a + b, key=lambda x: x[0])
-    result = []
-
-    for interval in intervals:
-        if not result or result[-1][1] < interval[0] - 1:
-            result.append(interval)  # Füge das Intervall direkt hinzu
-        else:
-            result[-1][1] = max(result[-1][1], interval[1])
-
-    return result
-
-def intersect(a,b):
-    for k in a:
-        a[k] = interval_intersection(a[k],b[k])
-    return a
-
-def union(a,b):
-    for k in a:
-        a[k] = interval_union(a[k],b[k])
-    return a
-
-def getPossib(p_org,s):
-    p_res = {'a':[], 'm':[], 'x':[], 's':[]}
-    for wf in workflows:
-        p = p_org.copy()
-        v=workflows[wf]
-        if s not in v:
-            continue
-        if wf!='in':
-            p=intersect(getPossib(p,wf),p)
-        for cond_n in v.split(',')[:-1]:
-            cond,n = cond_n.split(':')
-            inti = {'a':[[0,4000]], 'm':[[0,4000]], 'x':[[0,4000]], 's':[[0,4000]]}
-            if n==s:
-                if '>' in cond:
-                    inti[cond.split('>')[0]][0][0] = int(cond.split('>')[1])+1
-                elif '<' in cond:
-                    inti[cond.split('<')[0]][0][1] = int(cond.split('<')[1])-1
-                p = intersect(p,inti)
-                break
-            if '>' in cond:
-                inti[cond.split('>')[0]][0][1] = int(cond.split('>')[1])-1
-            elif '<' in cond:
-                inti[cond.split('<')[0]][0][0] = int(cond.split('<')[1])+1
-            p = intersect(p,inti)
-        p_res = union(p_res,p)
-        print(s,p)
-    return p_res
-
-possib_v = {'x':[[1,4000]], 'm':[[1,4000]], 'a':[[1,4000]], 's':[[1,4000]]}
-print(getPossib(possib_v,'A'))
-print(4000*4000*4000*((1350)+(2769-1352)+(3447-2771)+(3449-4000)))
-print(167409079868000)
+while queue:
+    s,inter = queue.pop(0)
+    conditional = workflows[s].split(',')[:-1]
+    last = workflows[s].split(',')[-1]   
+    possible = True 
+    for newS in conditional:
+        cond,n = newS.split(':')
+        temp = deepcopy(inter)
+        if '>' in cond:
+            i = idx[cond.split('>')[0]]
+            temp[i][0] = max(inter[i][0],int(cond.split('>')[1])+1)
+            inter[i][1] = min(inter[i][1],int(cond.split('>')[1])) 
+        elif '<' in cond:
+    
+            i = idx[cond.split('<')[0]] 
+            temp[i][1] = min(inter[i][1],int(cond.split('<')[1])-1)
+            inter[i][0] = max(inter[i][0],int(cond.split('<')[1]))
+        if n == 'A':
+            accepeted.append(temp)
+        elif n != 'R' and temp[i][0]<=temp[i][1]:
+            queue.append([n,temp])
+        if inter[i][0]>inter[i][1]:
+            possible = False
+            break
+    if not possible:
+        continue
+    if last == 'A':
+        accepeted.append(inter)
+    elif last != 'R':
+        queue.append([last,inter])
+count =0
+for a in accepeted:
+    c = 1
+    for i in range(4):
+        c *= a[i][1]-a[i][0]+1
+    count += c
+print(count)
