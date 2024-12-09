5ddb3e8e96c953054416cb3bd08d7255138d8523
coderJakub <kliemann.jakub@gmail.com>
2024-12-09 15:08:20 +0100

    day9 improved

--- 2024/day09.py
+++ 2024/day09.py
@@ -2,61 +2,65 @@ with open('in.txt') as f:
     lines = f.read()
 
 disk =[]
-id = 0
 for i in range(0,len(lines),2):
     if i>0:
         disk.append([-1,int(lines[i-1])])
-    disk.append([id,int(lines[i])])
-    id +=1
+    disk.append([i//2,int(lines[i])])
 
-p1 = disk.copy()
-p2 = disk.copy()        
+def orderPart1(disk: list):
+    res = []
+    
+    #Transferiert die Disk in der richtigen Reihenfolge in res und entfernt freie Plätze
+    while disk!=[]:
+        num, lenN = disk.pop(0)
+        if num!=-1:
+            res.append([num,lenN])
+        else:
+            while lenN>0 and disk!=[]:
+                if disk[-1][0] == -1:
+                    disk.pop()
+                else:
+                    num2, lenN2 = disk.pop()
+                    
+                    # Wenn der Platz für das File nicht reicht, restliches File wieder hinten anhängen
+                    if lenN2>lenN:
+                        disk.append([num2,lenN2-lenN])
+                        
+                    res.append([num2,min(lenN2,lenN)])
+                    lenN -= min(lenN2,lenN)
+    return res
 
-#-----------------Part 1-----------------
-count = 0
-k=0
-while p1!=[]:
-    num, lenN = p1.pop(0)
-    if num!=-1:
-        for i in range(lenN):
-            count += num*k
-            k+=1
-    else:
-        while lenN>0 and p1!=[]:
-            if p1[-1][0] == -1:
-                p1.pop()
-            else:
-                num2, lenN2 = p1.pop()
-                if lenN2>lenN:
-                    p1.append([num2,lenN2-lenN])
-                lenN2 = min(lenN2,lenN)
-                for i in range(lenN2):
-                    count += num2*k
-                    k+=1
-                lenN -= lenN2
-print(f'Part 1: {count}')
-
-
-#-----------------Part 2-----------------
-for i in range(len(p2)-1,0,-1):
-    if p2[i][0] == -1:
-        continue
-    size = p2[i][1]
-    for j in range(0,i):
-        if p2[j][0] != -1 or size>p2[j][1]:
+def orderPart2(disk: list):
+    #Geht von hinten durch die Disk durch und versucht die Files so weit wie möglich nach vorne zu schieben
+    for i in range(len(disk)-1,0,-1):
+        if disk[i][0] == -1:
             continue
-        num, size = p2.pop(i)
-        _,size2 = p2.pop(j)
-        p2.insert(j,[num,size])
-        p2.insert(i,[-1,size])
-        if size2>size:
-            p2.insert(j+1,[-1,size2-size])
-        break
-k=0
-count = 0
-for i,size in p2:
-    for _ in range(size):
-        if i!=-1:
-            count += i*k
-        k+=1
-print(f'Part 2: {count}')
+        size = disk[i][1]
+        
+        # Geht alle freien Plätze, welche vor dem File liegen durch und schaut ob das File dort reinpasst und schiebt es ggf. nach vorne
+        for j in range(0,i):
+            if disk[j][0] != -1 or size>disk[j][1]: #-> Entweder belegt oder nicht genug Platz
+                continue
+            num, size = disk.pop(i)
+            _,size2 = disk.pop(j)
+            
+            # Verschiebe das File nach vorne -> freier Platz an der Stelle des Files
+            disk.insert(j,[num,size])
+            disk.insert(i,[-1,size])
+            
+            #Wenn das File den freien Platz nicht komplett ausfüllt, wird der Rest als freier Platz hinter dem File angehängt
+            if size2>size:
+                disk.insert(j+1,[-1,size2-size])
+            break
+    return disk
+
+#Berechne checksum
+for p, orderedList in [('Part 1',orderPart1(disk.copy())),('Part 2',orderPart2(disk.copy()))]:
+    count = 0
+    k = 0
+    for i,size in orderedList:
+        for _ in range(size):
+            if i!=-1:
+                count += i*k
+            k+=1
+    print(f'{p}: {count}')
