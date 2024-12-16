840140aef5e1fbe328b52b3ef0c422afee134dd4
coderJakub <kliemann.jakub@gmail.com>
2024-12-16 14:11:39 +0100

    day16 p2

--- 2024/day16.py
+++ 2024/day16.py
@@ -1,5 +1,4 @@
 import heapq
-
 with open('in.txt') as f:
     grid = [[c for c in line] for line in f.read().splitlines()]
 
@@ -16,22 +15,40 @@ for i,line in enumerate(grid):
             grid[i][j] = '.'
 
 DIR = {'U': (-1,0), 'D': (1,0), 'L': (0,-1), 'R': (0,1)}
+LEFT_RIGHT = {'U': ('L','R'), 'D': ('R','L'), 'L': ('D','U'), 'R': ('U','D')}
 
 def bfs(start, end):
-    q = [(0, start, '',[start])]
+    q = [start[0],start[1]]
     heapq.heapify(q)
     visited = {}
+    best = 0
+    
     while q:
-        dist, (i,j), d, p = heapq.heappop(q)
-        if end == (i,j):
-            return dist
-        if (i,j) in visited.keys():
+        dist, (i,j), d= heapq.heappop(q)
+        if end == (i,j) and best==0:
+            best = dist
+        if (i,j,d) in visited.keys():
             continue
-        visited[(i,j)] = dist
-        for k,v in DIR.items():
-            ni, nj = i+v[0], j+v[1]
-            if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] != '#':
-                heapq.heappush(q, (dist+(1 if d==k else 1001), (ni,nj), k, p+[(ni,nj)]))
+        visited[(i,j,d)] = dist
+        ni, nj = i+DIR[d][0], j+DIR[d][1]
+        if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[ni][nj] != '#':
+            heapq.heappush(q, (dist+1, (ni,nj), d))
+        for new_d in LEFT_RIGHT[d]:
+            heapq.heappush(q, (dist+1000, (i,j), new_d))
+    return best, visited
                     
-p1 = bfs(start, end)
-print(p1)
+p1,vs = bfs([(1000, start, 'U'), (0, start, 'R')], end)
+
+#Go backwards
+DIR = {'U': (1,0), 'D': (-1,0), 'L': (0,1), 'R': (0,-1)}
+_,ve = bfs([(0, end, 'U'), (0, end, 'R')], start)
+
+shortestPaths = set()
+for i in range(R):
+    for j in range(C):
+        for d in DIR.keys():
+            if (i,j,d) in vs and (i,j,d) in ve and vs[(i,j,d)] + ve[(i,j,d)] == p1:
+                shortestPaths.add((i,j))
+                
+print(f'Part 1: {p1}')
+print(f'Part 2: {len(shortestPaths)}')
