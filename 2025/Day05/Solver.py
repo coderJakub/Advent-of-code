5c0b0d1f03c3d0b2aeea302da58cd7bea41d1e06
coderJakub <kliemann.jakub@gmail.com>
2025-12-05 12:36:42 +0100

    day5/2025

---
+++ 2025/Day05/Solver.py
@@ -0,0 +1,29 @@
+import sys
+
+filename = "in.txt" if len(sys.argv)<1 else sys.argv[1]
+with open(filename) as f:
+    grid = f.read().split('\n\n')
+
+fresh = [(tuple(map(int,line.split('-')))) for line in grid[0].splitlines()]
+avialable = list(map(int, grid[1].splitlines()))
+
+p1 = len(set(ingr for ingr in avialable for rg in fresh if rg[0]<=ingr<=rg[1]))
+
+while True:
+    newFresh = []
+    for of in fresh:
+        for i,nf in enumerate(newFresh):
+            o1, o2 = of
+            n1, n2 = nf
+            if (n1<=o1<=n2) or (o1<=n1<=o2):
+                newFresh[i] = (min(o1, n1), max(o2, n2))
+                break
+        else:
+            newFresh.append(of)
+    if newFresh == fresh: break
+    fresh = newFresh.copy()
+
+p2 = sum(i2-i1+1 for i1, i2 in fresh)
+
+print(f'Part 1: {p1}')
+print(f'Part 2: {p2}')
